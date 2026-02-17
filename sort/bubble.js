c2a625bf37742915d1ce9dc7e161cefaebd9d34e
Ivan Datsiv <ivandatsivmail@gmail.com>
2026-02-17 15:14:05 +0000

    Add bubble sort

---
+++ sort/bubble.js
@@ -0,0 +1,15 @@
+const bubbleSort = (array) => {
+  const n = array.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
+};
+
+const numbers = [64, 34, 25, 12, 22, 11, 90];
+console.log(bubbleSort(numbers));
